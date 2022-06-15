299c0350f9437c886085b32721b2c8acfcfd18d3
MURAKI Taichi <muraki.taichi@gmail.com>
2022-06-15 13:23:17 +0900

    f

--- src/tools/augmentation_data.py
+++ src/tools/augmentation_data.py
@@ -13,6 +13,12 @@ def load(json_file_path):
 
 
 # OUTPUT_DATA_DIR =
+def _iter_coco_anno(im_list, anno_list):
+    assert len(im_list) == len(anno_list)
+    for im, anno in zip(im_list, anno_list):
+        assert im["id"] == anno["image_id"]
+        # file_name = im["file_name"]
+        yield im, anno
 
 
 def main(base_dir):
@@ -21,6 +27,8 @@ def main(base_dir):
     d = load(coco_json)
     im_list = d["images"]
     anno_list = d["annotations"]
+    for im, anno in _iter_coco_anno(im_list, anno_list):
+        print(im)
 
 
 if __name__ == "__main__":
