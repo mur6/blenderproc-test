9e861f29b23fa76246536d53bfc587318dc8d2b8
MURAKI Taichi <muraki.taichi@gmail.com>
2022-06-15 13:21:30 +0900

    f

--- src/tools/augmentation_data.py
+++ src/tools/augmentation_data.py
@@ -15,12 +15,14 @@ def load(json_file_path):
 # OUTPUT_DATA_DIR =
 
 
-def main():
-    coco_json = pathlib.Path("augmentation/mathand_train.json")
+def main(base_dir):
+    image_data_dir = base_dir / "mathand" / "images"
+    coco_json = base_dir / "mathand_train.json"
     d = load(coco_json)
     im_list = d["images"]
     anno_list = d["annotations"]
 
 
 if __name__ == "__main__":
-    main()
+    base_dir = sys.argv[1]
+    main(pathlib.Path(base_dir))
