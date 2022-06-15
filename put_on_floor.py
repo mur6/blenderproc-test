ff79f5b12b3d2ec760845d98e4c7e7cbef7a5c51
MURAKI Taichi <muraki.taichi@gmail.com>
2022-06-15 10:31:27 +0900

    Fix only format

--- put_on_floor.py
+++ put_on_floor.py
@@ -12,9 +12,9 @@ import mathutils
 from bpy_extras.object_utils import world_to_camera_view
 
 
-
 def create_room_ground():
     return bproc.object.create_primitive("PLANE", scale=[5, 5, 1])
+
 
 def make_light_2():
     # define a light and set its location and energy level
@@ -22,7 +22,7 @@ def make_light_2():
     light.set_type("POINT")
     light.set_location([5, -5, 5])
     light.set_color([1, 1, 1])
-    light.set_energy(1200)#random.uniform(100, 550))
+    light.set_energy(1200)  # random.uniform(100, 550))
 
 
 def make_target_mat_obj():
@@ -34,6 +34,7 @@ def make_target_mat_obj():
     obj.set_scale([0.42, 0.42, 0.25])
     obj.set_cp("category_id", 1)
     return obj
+
 
 def make_mat_keypoints():
     p1 = [0.0, 0.218, 0.002]
@@ -55,6 +56,7 @@ def set_resolution_and_get_render_size():
 
 
 OUTPUT_DATA_DIR = pathlib.Path("data/outputs/coco")
+
 
 def _write_keypoints(*, output_dir, list_of_keypoints):
     js_str = json.dumps(list_of_keypoints, indent=4)
@@ -92,9 +94,13 @@ def sample_random_camera(ground, poi, keypoints_builder, texture):
         # Sample random camera location above objects
         location = np.random.uniform([-1.35, -1.35, 1.95], [1.35, 1.35, 1.95])
         # Compute rotation based on vector going from location towards poi
-        rotation_matrix = bproc.camera.rotation_from_forward_vec(poi - location, inplane_rot=np.random.uniform(-0.7854, 0.7854))
+        rotation_matrix = bproc.camera.rotation_from_forward_vec(
+            poi - location, inplane_rot=np.random.uniform(-0.7854, 0.7854)
+        )
         # Add homog cam pose based on location an rotation
-        cam2world_matrix = bproc.math.build_transformation_mat(location, rotation_matrix)
+        cam2world_matrix = bproc.math.build_transformation_mat(
+            location, rotation_matrix
+        )
         bproc.camera.add_camera_pose(cam2world_matrix)
         keypoints = keypoints_builder(bpy.context.scene, bpy.context.scene.camera)
         list_of_keypoints.append(keypoints)
@@ -125,15 +131,18 @@ def main():
                 coord_2d = world_to_camera_view(scene, camera, keypoints_coord)
                 coord_2d = _convert_camera_coord(coord_2d, render_size=render_size)
                 yield coord_2d
+
         lis = [[*tp, 2] for tp in _iter_tpls()]
         return reduce(operator.add, lis, [])
 
     for count, texture in enumerate(cc_textures[:8], 1):
-        list_of_keypoints = sample_random_camera(ground, poi, keypoints_builder, texture)
+        list_of_keypoints = sample_random_camera(
+            ground, poi, keypoints_builder, texture
+        )
         _render_and_save(count=count, list_of_keypoints=list_of_keypoints)
         bproc.utility.reset_keyframes()
         time.sleep(8)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
